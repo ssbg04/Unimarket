9dc6564211fccfd4b92a161b56d7ec258167c34f
ssbg04 <143066036+ssbg04@users.noreply.github.com>
2025-06-23 23:21:59 +0800

    Update auth_functions.php

--- includes/auth_functions.php
+++ includes/auth_functions.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '../config/database.php';
 
 /**
  * Check if a user is logged in
