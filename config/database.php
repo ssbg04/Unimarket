306f849127202bbc70578e59373b63bf2d97d1c6
ssbg04 <143066036+ssbg04@users.noreply.github.com>
2025-06-23 22:45:52 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,14 +1,13 @@
 <?php
 $host = getenv("MYSQLHOST");
-$port = getenv("MYSQLPORT");
 $user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
+$password = getenv("MYSQLPASSWORD");
+$db = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
+$conn = new mysqli($host, $user, $password, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected to MySQL on Railway!";
 ?>
