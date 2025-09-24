c3e42d576aac0f23d22648917e000d2235587229
ssbg04 <143066036+ssbg04@users.noreply.github.com>
2025-09-24 13:41:52 +0800

    Delete database.sql

--- database.sql
+++
@@ -1,7 +0,0 @@
--- Truncate orders table and reset auto-increment
-TRUNCATE TABLE orders;
-TRUNCATE TABLE order_items;
-ALTER TABLE orders AUTO_INCREMENT = 1;
-ALTER TABLE order_items AUTO_INCREMENT = 1;
-
-ALTER TABLE cart_items ADD COLUMN size VARCHAR(10) DEFAULT NULL; 
